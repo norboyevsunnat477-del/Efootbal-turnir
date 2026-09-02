c43173ac2ee2dff465a04cce46a6db97f567ccaa
norboyevsunnat477-del <norboyevsunnat477@gmail.com>
2026-09-02 21:09:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
-BOT_TOKEN = "BOT_TOKENINI_SHUYERGA_YOZING"
-ADMIN_ID = 123456789  
-CHANNEL_USERNAME = "@kanalingiz_username"
+BOT_TOKEN = "8802613886:AAF9SvRntPSB8b1GXaNrWFy1zGJiBa7_NP8"
+ADMIN_ID = 5244022908  
+CHANNEL_USERNAME = "@efmobileuz"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
